89abdb31537ce143a33726b2c14eccbc485e3702
Monu_2114 <monu.nanabala@gmail.com>
2025-12-15 00:52:48 +0530

    stored chunks and embeddings into supabase !!

--- data-ingestion-pipeline/ingest.js
+++
@@ -1,9 +0,0 @@
-import { readFileSync } from "fs";
-const readFile = async () => {
-  const text = readFileSync(
-    "data/03 Harry Potter and the Prisoner of Azkaban.txt",
-    "utf-8"
-  );
-  console.log(text);
-};
-readFile();
---
+++ data-ingestion-pipeline/ingestion.py
@@ -0,0 +1,39 @@
+# get files from data folder
+# read files
+# chunk files
+# generate embeddings
+# insert into db
+from pathlib import Path
+from dotenv import load_dotenv
+from sentence_transformers import SentenceTransformer
+import os
+from supabase import create_client, Client
+load_dotenv()
+model = SentenceTransformer("google/embeddinggemma-300m")
+
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+
+def chunk_text(file,chunk_size=500):
+    text=file.read_text(encoding="utf-8")
+    for i in range(0,len(text),chunk_size):
+        yield text[i:i+chunk_size]
+       
+
+path=Path("data")
+files=path.glob("**/*.txt")
+rows=[] #batch embeddings and insert
+for file in files:
+    chunks=chunk_text(file)
+    for chunk in chunks:
+        embeddings=model.encode(chunk).tolist()
+        rows.append({"content": chunk, "embedding": embeddings})
+        if(len(rows)==50):
+            supabase.table("documents").insert(rows).execute()
+            rows=[]
+if rows:
+    supabase.table("documents").insert(rows).execute()
+        
